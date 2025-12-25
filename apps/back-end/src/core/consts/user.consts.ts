d79e6e4238374bbd2aad60d4b23f1112d5408b55
Jasulan <zhasik7002@gmail.com>
2025-12-25 23:39:26 +0500

    Delete consts directory

--- apps/back-end/src/core/consts/user.consts.ts
+++
@@ -1,6 +0,0 @@
-import { RoleEnum } from '../db/enums/role.enum';
-
-export const users = [
-  { email: 'almas@gmail.com', role: RoleEnum.TEACHER },
-  { email: 'maksat@gmail.com', role: RoleEnum.ADMIN },
-];
